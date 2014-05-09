827369c78bd2c5be55c48ee17263e1e1b9cbcbce
gbarandu <gbarandu@student.ethz.ch>
2014-05-09 11:39:39 +0200

    Changes

--- code/global_pheromene_update.m
+++ code/global_pheromene_update.m
@@ -3,13 +3,13 @@
 %Verbindungsstrecke zwischen den einzelnen cities zusätzlich Pheromen
 %deponiert.
 %
-%Input:     Vektor der kürzesten Tour von der trajectory-Matrix, ganze
+%Input:     Vektor der kürzesten Tour von der trajectory-Matrix !!!ALS VEKTOR!!, ganze
 %           Matrix tau, Länge der kürzesten Tour, Anzahl Städte, Parameter
 %Output:    Aktuelle Pheromenwerte in tau
 
-function [tau] = global_pheromene_update( trajectory(:,shortest_path_index), tau, shortest_path, no_cities, alpha )
+function [tau] = global_pheromene_update(trajectory, tau, shortest_path, no_cities, alpha )
 
-    for ii = 1:no_cities
+    for ii = 1:no_cities-1
 
             if (trajectory(ii) > trajectory(ii+1))
                 tau(trajectory(ii+1), trajectory(ii)) = (1-alpha)*tau(trajectory(ii+1),trajectory(ii))+alpha/shortest_path;
@@ -18,5 +18,8 @@ function [tau] = global_pheromene_update( trajectory(:,shortest_path_index), tau
                 tau(trajectory(ii), trajectory(ii+1)) = (1-alpha)*tau(trajectory(ii),trajectory(ii+1))+alpha/shortest_path;
 
             end    
+    end
+    
+    tau (1, trajectory(no_cities)) = (1-alpha)*tau(1,trajectory(no_cities))+alpha/shortest_path;
 end
 
