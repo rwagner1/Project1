64f14ff1ad16e48c928f9f44fbd9e61c9c72b2b4
rwagner1 <raphaela7@hispeed.ch>
2014-05-11 16:05:24 +0200

    ich buchstabier dir deinen namen

--- other/R-Skripte/rounds_vs_shortestpathminima.R
+++ other/R-Skripte/rounds_vs_shortestpathminima.R
@@ -12,7 +12,7 @@ rm(list = ls())
 #Daten einlesen
 rounds = c(200,400,600,800,1000,1200,1400,1600,1800,2000)
 global_shortest_path_average.V1 = c(487.4028,442.3497,442.7367,432.3335,434.8084, 436.0793, 434.7726, 431.2449,429.7371, 429.5303)
-global_shortest_path_average.V2 = c(505.4319,460.0544,442.8916,442.4961,439.4602,436.5999,0,0, 430.3957,429.5303)
+global_shortest_path_average.V2 = c(482.4596, 446.6279,434.8789,431.3724,433.2166,431.9794,431.9479,431.3109, 430.3957,429.5303)
 errors.V1 = c(10.4641,8.8395,4.8312,6.1660,3.3879,2.9796,3.3960,3.3416,2.6631,2.8202)                        #Standardabweichung
 errors.V2 = c(10.7538,8.7496,6.1061,6.9268,3.9622,3.2160,1,1, 2.5062,3.1934)
 
@@ -22,7 +22,7 @@ plot(rounds,global_shortest_path_average.V1,type="n",
      xlim=c(50,2100),
      ylim=c(400,550),
      xlab=expression(paste("rounds"," / ", a.u.)),
-     ylab=expression(paste("<shortest path>"," / ", a.u.)),
+     ylab=expression(paste("minimal shortest path"," / ", a.u.)),
      
 )
 y.errorbars.V1 <- function(rounds,global_shortest_path_average.V1,errors.V1,ebl=0.02){arrows(rounds,global_shortest_path_average.V1-errors.V1,rounds,global_shortest_path_average.V1+errors.V1,code=3,angle=90,length=ebl)}
